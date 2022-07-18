f5d70dcf1211ef6719697ac13118f2429e5a8b8b
Promise Okeke <plastamasta21@gmail.com>
2022-07-18 02:12:44 +0100

    changed the javascript program to a new one cuz the last one looked awfull

--- index.js
+++ index.js
@@ -1,48 +1,75 @@
-const myBtn = document.getElementById("rb1");
-const myBtn2 = document.getElementById("rb2");
-const myBtn3 = document.getElementById("rb3");
-const myBtn4 = document.getElementById("rb4");
-const myBtn5 = document.getElementById("rb5");
-const submitBtn = document.getElementById("submitButton");
+// const myBtn = document.getElementById("rb1");
+// const myBtn2 = document.getElementById("rb2");
+// const myBtn3 = document.getElementById("rb3");
+// const myBtn4 = document.getElementById("rb4");
+// const myBtn5 = document.getElementById("rb5");
+// const submitBtn = document.getElementById("submitButton");
+// const mainContainer = document.getElementById("container");
+// const thanksSection = document.getElementById("thanks");
+// const ouputValue = document.getElementById("selected");
+
+// var rateValue;
+// myBtn.addEventListener("click", () => {
+//   myBtn.style.backgroundColor = "#959eac";
+//   myBtn.style.color = "white";
+//   rateValue = this.length + 1;
+// });
+// myBtn2.addEventListener("click", () => {
+//   myBtn2.style.backgroundColor = "#959eac";
+//   myBtn2.style.color = "white";
+//   rateValue = this.length + 2;
+// });
+// myBtn3.addEventListener("click", () => {
+//   myBtn3.style.backgroundColor = "#959eac";
+//   myBtn3.style.color = "white";
+//   rateValue = this.length + 3;
+// });
+// myBtn4.addEventListener("click", () => {
+//   myBtn4.style.backgroundColor = "#959eac";
+//   myBtn4.style.color = "white";
+//   rateValue = this.length + 4;
+// });
+// myBtn5.addEventListener("click", () => {
+//   myBtn5.style.backgroundColor = "#959eac";
+//   myBtn5.style.color = "white";
+//   rateValue = this.length + 5;
+// });
+
+// submitBtn.addEventListener("click", () => {
+//   if (rateValue == null) {
+//     alert(
+//       "Please rate us so we can maybe eat something other than frozen pizzas, ulcer is not good at all, i will tell you that"
+//     );
+//   } else {
+//     ouputValue.textContent = `${rateValue}`;
+//     mainContainer.style.display = "none";
+//     thanksSection.style.display = "block";
+//   }
+// });
 const mainContainer = document.getElementById("container");
 const thanksSection = document.getElementById("thanks");
 const ouputValue = document.getElementById("selected");
+const rateBtn = document.querySelectorAll(".rateBtn");
+const rb = document.querySelectorAll(".rb");
+const submitBtn = document.getElementById("submitButton");
+var output;
 
-var rateValue;
-myBtn.addEventListener("click", () => {
-  myBtn.style.backgroundColor = "#959eac";
-  myBtn.style.color = "white";
-  rateValue = this.length + 1;
-});
-myBtn2.addEventListener("click", () => {
-  myBtn2.style.backgroundColor = "#959eac";
-  myBtn2.style.color = "white";
-  rateValue = this.length + 2;
-});
-myBtn3.addEventListener("click", () => {
-  myBtn3.style.backgroundColor = "#959eac";
-  myBtn3.style.color = "white";
-  rateValue = this.length + 3;
-});
-myBtn4.addEventListener("click", () => {
-  myBtn4.style.backgroundColor = "#959eac";
-  myBtn4.style.color = "white";
-  rateValue = this.length + 4;
-});
-myBtn5.addEventListener("click", () => {
-  myBtn5.style.backgroundColor = "#959eac";
-  myBtn5.style.color = "white";
-  rateValue = this.length + 5;
+rateBtn.forEach((btn) => {
+  btn.addEventListener("click", (b) => {
+    btn.style.backgroundColor = "#959eac";
+    btn.style.color = "white";
+    output = b.currentTarget.id;
+  });
 });
 
 submitBtn.addEventListener("click", () => {
-  if (rateValue == null) {
+  if (output === undefined) {
     alert(
       "Please rate us so we can maybe eat something other than frozen pizzas, ulcer is not good at all, i will tell you that"
     );
   } else {
-    ouputValue.textContent = `${rateValue}`;
+    ouputValue.textContent = `${output}`;
     mainContainer.style.display = "none";
-    thanksSection.style.display = "block";
+    thanksSection.style.display = "flex";
   }
 });
